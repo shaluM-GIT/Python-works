b2e84c1d2a66c792d4d8fc237a4cfacc2112bc61
shaluM-GIT <shaldezire@gmail.com>
2026-09-11 19:55:09 -0700

    strongly typed.py

---
+++ strongly typed.py
@@ -0,0 +1,10 @@
+
+salary = '50000'
+bonus = 10000
+print('Total Salary in Inceptez:', salary + bonus)
+throwing error showing its strongly typed
+
+salary = 50000
+bonus = 10000
+print('Total Salary in Inceptez:', salary + bonus)
+int+int will work
