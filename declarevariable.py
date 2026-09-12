6c9c15a92bf0c5590e880e930807c7e4f9d49959
shaluM-GIT <shaldezire@gmail.com>
2026-09-11 19:49:25 -0700

    declarevariable.py

---
+++ declarevariable.py
@@ -0,0 +1,8 @@
+DataEngineeringBatch ='Big Data & Cloud'      
+print(DataEngineeringBatch)                   
+                                              
+dataEngineeringBatch ='AI ENGINEERING'        
+print(dataEngineeringBatch)                   
+                                              
+data_engineering_batch ="PYTHON"              
+print(data_engineering_batch)                 
