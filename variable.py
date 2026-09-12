2c9e497c48cd0c37abb8ec9111c3f387ae2fe4b3
shaluM-GIT <shaldezire@gmail.com>
2026-09-11 19:42:40 -0700

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,4 @@
+Student_Name ='arun'                                                                                    
+Course_Name = 'Python Fundamentals'                                                                     
+Training_Institute_Name = 'Inceptez Technologies'                                                       
+print (Student_Name, 'is learning course', Course_Name ,'in', Training_Institute_Name)                  
